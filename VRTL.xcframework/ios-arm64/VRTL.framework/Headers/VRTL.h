50d3c0599e7d5bfe0ae1c1328356534083c4a6b9
Fahad Ahmed Usmani <fahad.usmani@nymcard.com>
2023-03-29 12:55:39 +0500

    Added SDK

---
+++ VRTL.xcframework/ios-arm64/VRTL.framework/Headers/VRTL.h
@@ -0,0 +1,18 @@
+//
+//  VRTL.h
+//  VRTL
+//
+//  Created by Fahad Ahmed Usmani on 27/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VRTL.
+FOUNDATION_EXPORT double VRTLVersionNumber;
+
+//! Project version string for VRTL.
+FOUNDATION_EXPORT const unsigned char VRTLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VRTL/PublicHeader.h>
+
+
