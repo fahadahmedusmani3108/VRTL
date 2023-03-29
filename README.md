b240e61701e064f5c58c27229735756a1a3452d7
Fahad Ahmed Usmani <fahad.usmani@nymcard.com>
2023-03-29 17:05:59 +0500

    Package updated

---
+++ README.md
@@ -0,0 +1,3 @@
+# VRTLSDK
+
+A description of this package.
---
+++ Tests/VRTLSDKTests/VRTLSDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VRTLSDK
+
+final class VRTLSDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VRTLSDK().text, "Hello, World!")
+    }
+}
